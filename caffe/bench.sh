473e574327e5957207583c5726dba8c9de7f9900
DGX Admin <dgxadmin@sdc-1.samsungdisplay.com>
2017-11-10 21:03:45 +0900

    code freeze for caffe

--- caffe/bench.sh
+++ caffe/bench.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 COMMAND="bash ./run_model.sh"
 
-${COMMAND} inception_v1 1 64 1 fake /data/ilsvrc12_train_lmdb /data/ilsvrc12_val_lmdb /data/imagenet_mean.binaryproto
-${COMMAND} vgg_d 1 64 1 real /data/ilsvrc12_train_lmdb /data/ilsvrc12_val_lmdb /data/imagenet_mean.binaryproto
-${COMMAND} resnet_50 1 64 1 real /data/ilsvrc12_train_lmdb /data/ilsvrc12_val_lmdb /data/imagenet_mean.binaryproto
-${COMMAND} resnet_101 1 64 1 real /data/ilsvrc12_train_lmdb /data/ilsvrc12_val_lmdb /data/imagenet_mean.binaryproto
+${COMMAND} inception_v1 1 64 8 fake /data/ilsvrc12_train_lmdb /data/ilsvrc12_val_lmdb /data/imagenet_mean.binaryproto
+${COMMAND} resnet_101 1 32 8 fake /data/ilsvrc12_train_lmdb /data/ilsvrc12_val_lmdb /data/imagenet_mean.binaryproto
