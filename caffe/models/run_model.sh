bec6e1d611c8acb1e10102e36c535e72e5643839
DGX Admin <dgxadmin@sdc-1.samsungdisplay.com>
2017-11-11 14:58:11 +0900

    model clean-up

--- caffe/models/run_model.sh
+++ caffe/models/run_model.sh
@@ -14,29 +14,6 @@ VERBOSE_RUN=${VERBOSE_RUN:-0}
 SAMPLE_SIZE=1281167
 MAXGPUS="$(nvidia-smi -L | wc -l)"
 
-#trap ctrl-c and call cleanup
-trap cleanup SIGINT SIGTERM SIGHUP
-
-trap failure SIGABRT SIGSEGV
-
-cleanup() {
-    rm -f ${TRAIN_VAL_FILE} ${SOLVER_FILE}
-    rm -f *.solverstate *.caffemodel
-}
-
-failure() {
-    mkdir -p ${FAIL_DIR}
-    mv $LOG_FILE ${FAIL_DIR}/
-}
-
-check_gpus() {
-    if [[ ${NGPUS} > $MAXGPUS ]]; then
-        echo "========== NGPUS=${NGPUS} > MAXGPUS($MAXGPUS), SKIPPING!!! ========"
-        cleanup
-        exit 1
-    fi
-}
-
 case "$NGPUS" in
         1) GPU_OPT=--gpu=0
         ;;
@@ -49,9 +26,9 @@ case "$NGPUS" in
 esac
 
 MAXITER=$((${SAMPLE_SIZE} * ${EPOCHS} / ${BATCHSIZE} / ${NGPUS}))
+MAXITER=1000
 
-FAIL_DIR=${LOG_DIR:-"${RESULTS_DIR}/failure_logs"}
-MODEL_DIR="$(pwd)/models/${MODEL}"
+MODEL_DIR="./models/${MODEL}"
 
 CAFFE_BIN=${CAFFE_BIN:-"/usr/local/bin/caffe"}
 
@@ -72,33 +49,17 @@ else
     TRAIN_BATCHSIZE=$((BATCHSIZE*NGPUS))
 fi
 
-# Caffe-exp supports GPU trnasform for data layer.
-if [[ $DATA_TRANSFORM == gpu ]]; then
-    GPU_TRANSFORM="\nuse_experimental_gpu_transform: true"
-else
-    GPU_TRANSFORM=""
-fi
+cleanup() {
+    rm -f ${TRAIN_VAL_FILE} ${SOLVER_FILE}
+    rm -f *.solverstate *.caffemodel
+}
 
 # Assuming that @ is not used in train_val files
 cat ${TRAIN_TMPL_FILE} | sed "s/REPLACEBATCHSIZE/${TRAIN_BATCHSIZE}/g" | sed "s@REPLACEIMAGENETMEAN@${IMAGENET_MEAN}@g" | sed "s@REPLACEIMAGENETTRAIN@${IMAGENET_TRAIN}@g" | sed "s@REPLACEIMAGENETVAL@${IMAGENET_VAL}@g" | sed "s@transform_param {@transform_param {${GPU_TRANSFORM}@g" > ${TRAIN_VAL_FILE}
 
 cat ${SOLVER_TMPL_FILE} | sed "s/REPLACEBATCHSIZE/${BATCHSIZE}/g" | sed "s/REPLACEMAXITER/${MAXITER}/g" | sed "s^REPLACEMODELDIR^${MODEL_DIR}^g" | sed "s^REPLACEDATAKIND^${DATA_KIND}_data^g" > ${SOLVER_FILE}
 
-if [[ $VERBOSE_RUN != 0 ]]; then
-    START_TIME=$(date)
-    echo "===== STARTING MODEL: ${MODEL}  ITERATIONS:${MAXITER} BATCH SIZE:${BATCHSIZE} GPUS:${NGPUS} ON ${START_TIME} ====="
-fi
-
-check_gpus
-
 mkdir -p ${LOG_DIR}
 ${CAFFE_BIN}  train --solver=${SOLVER_FILE} ${GPU_OPT} 2>&1 | tee ${LOG_FILE}
-
-if [[ $VERBOSE_RUN != 0 ]]; then
-    END_TIME=$(date)
-    echo "===== FINISHED MODEL: ${MODEL}  ITERATIONS:${MAXITER} BATCH SIZE:${BATCHSIZE} GPUS:${NGPUS} ON ${END_TIME} ====="
-    DURATION=$(expr $(date -u -d "$END_TIME" +"%s") - $(date -u -d "$START_TIME" +"%s"))
-    echo "&&&& PERF time $DURATION -secs"
-fi
 
 cleanup
---
+++ caffe/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# NVIDIA Copyright
+
+if [ -z "$1" ]; then
+  echo "Usage: docker-run.sh [dataset]"
+  exit
+fi
+
+# Create the output and temporary directories.
+DATASET_DIR="$1"
+BENCHMARK_SCRIPT_DIR="$(pwd)"
+VERSION=17.10
+
+nvidia-docker run --rm -ti --name caffe \
+    -u $(id -u):$(id -g) \
+    --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
+    -v ${DATASET_DIR}:/imagenet \
+    -v ${BENCHMARK_SCRIPT_DIR}:/workspace \
+    nvcr.io/nvidia/caffe:$VERSION bash bench.sh
+
+
