411efc4afc5da5e5c97a000f5bd1187befc4eabf
DGX Admin <dgxadmin@sdc-1.samsungdisplay.com>
2017-11-12 14:23:48 +0900

    fix input size

--- caffe/bench.sh
+++ caffe/bench.sh
@@ -2,4 +2,5 @@
 COMMAND="bash ./models/run_model.sh"
 
 ${COMMAND} inception_v1 1 64 8 real /imagenet/ilsvrc12_train_lmdb /imagenet/ilsvrc12_val_lmdb /imagenet/imagenet_mean.binaryproto
+${COMMAND} inception_v1 1 96 8 real /imagenet/ilsvrc12_train_lmdb /imagenet/ilsvrc12_val_lmdb /imagenet/imagenet_mean.binaryproto
 ${COMMAND} resnet_101 1 32 8 real /imagenet/ilsvrc12_train_lmdb /imagenet/ilsvrc12_val_lmdb /imagenet/imagenet_mean.binaryproto
