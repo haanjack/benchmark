26d61dea03c0dc6353ed12ef7adba2991c317d88
DGX Admin <dgxadmin@sdc-1.samsungdisplay.com>
2017-11-12 14:38:24 +0900

    Fixed epochs setting

--- caffe/bench.sh
+++ caffe/bench.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 COMMAND="bash ./models/run_model.sh"
+NUM_EPOCHS=1
 
-${COMMAND} inception_v1 1 64 8 real /imagenet/ilsvrc12_train_lmdb /imagenet/ilsvrc12_val_lmdb /imagenet/imagenet_mean.binaryproto
-${COMMAND} inception_v1 1 96 8 real /imagenet/ilsvrc12_train_lmdb /imagenet/ilsvrc12_val_lmdb /imagenet/imagenet_mean.binaryproto
-${COMMAND} resnet_101 1 32 8 real /imagenet/ilsvrc12_train_lmdb /imagenet/ilsvrc12_val_lmdb /imagenet/imagenet_mean.binaryproto
+${COMMAND} inception_v1 ${NUM_EPOCHS} 64 8 real /imagenet/ilsvrc12_train_lmdb /imagenet/ilsvrc12_val_lmdb /imagenet/imagenet_mean.binaryproto
+${COMMAND} inception_v1 ${NUM_EPOCHS} 96 8 real /imagenet/ilsvrc12_train_lmdb /imagenet/ilsvrc12_val_lmdb /imagenet/imagenet_mean.binaryproto
+${COMMAND} resnet_101 ${NUM_EPOCHS} 32 8 real /imagenet/ilsvrc12_train_lmdb /imagenet/ilsvrc12_val_lmdb /imagenet/imagenet_mean.binaryproto
