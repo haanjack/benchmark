27145a347ccdc8e354b39c7c9f2b12488a5785d5
DGX Admin <dgxadmin@sdc-1.samsungdisplay.com>
2017-11-17 16:27:47 +0900

    testing distributed training code

--- tensorflow/bench.sh
+++ tensorflow/bench.sh
@@ -5,22 +5,30 @@ LOG_DIR=output
 TIMESTAMP=$(date +%m%d%H%M)
 NUM_EPOCHS=100
 NUM_GPU=8
-VARIABLE_UPDATE=replicated
+VARIABLE_UPDATE=distributed_replicated
+JOB_NAME=$1
+TASK_INDEX=$2
 
 mkdir -p ${LOG_DIR}
 
 train() {
     MODEL=$1
     BATCH_SIZE=$2
+    JOB_NAME=$3
+    TASK_INDEX=$4
     TIMESTAMP=$(date +%m%d%H%M)
     LOG_FILE="${LOG_DIR}/output_${MODEL}_e${NUM_EPOCHS}_b${BATCH_SIZE}.${TIMESTAMP}.log"
     python tf_cnn_benchmarks/tf_cnn_benchmarks.py \
+        --job_name=${JOB_NAME} \
+        --ps_hosts="192.168.200.212:2222" \
+        --worker_hosts="192.168.200.212:2222" \
+        --task_index=${TASK_INDEX} \
         --model=${MODEL} --batch_size=${BATCH_SIZE} --num_baches=${NUM_EPOCHS} --num_gpus=${NUM_GPU} \
         --data_name=imagenet --data_dir=${DATA_DIR} --variable_update=${VARIABLE_UPDATE} \
         2>&1 | tee ${LOG_FILE}
 	
 }
 
-train googlenet 64
-train resnet101 32
+train googlenet 64 ${JOB_NAME} ${TASK_INDEX}
+#train resnet101 32
 
--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 # NVIDIA Copyright
 
+DATASET_DIR="$1"
+JOB_NAME="$2"
+TASK_INDEX=$3
+
 if [ -z "$1" ]; then
   echo "Usage: docker-run.sh [dataset]"
   exit
 fi
 
 # Create the output and temporary directories.
-DATASET_DIR="$1"
 BENCHMARK_SCRIPT_DIR="$(pwd)"
 VERSION=17.10
 
-nvidia-docker run --rm -ti --name tensorflow \
+nvidia-docker run --rm -ti --name tensorflow-${JOB_NAME} \
     -u $(id -u):$(id -g) \
     --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 \
+    -p 2222:2222 \
     -v ${DATASET_DIR}:/imagenet \
     -v ${BENCHMARK_SCRIPT_DIR}:/workspace \
-    nvcr.io/nvidia/tensorflow:$VERSION 
+    nvcr.io/nvidia/tensorflow:$VERSION bash bench.sh ${JOB_NAME} ${TASK_INDEX}
 
 
 
