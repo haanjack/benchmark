5f6b80e2fdd6f9e48ae910634912b2ba4b89c766
Jack Han <jahan@nvidia.com>
2018-03-16 12:30:24 +0900

    update caffe benchmark

--- caffe/models/run_model.sh
+++ caffe/models/run_model.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
 MODEL="$1"
-EPOCHS="$2"
-BATCHSIZE="$3"
-NGPUS=${4:-0}
-DATA_KIND="$5"
-IMAGENET_TRAIN="$6"
-IMAGENET_VAL="$7"
-IMAGENET_MEAN="$8"
+MAXITER="$2"
+BATCHSIZE="${3:-32}"
+FP16=${4:-0}
+NGPUS=${5:-1}
+IMAGENET_TRAIN="${6:-/imagenet/ilsvrc12_train_lmdb}"
+IMAGENET_VAL="${7:-/imagenet/ilsvrc12_val_lmdb}"
+IMAGENET_MEAN="${8:-/imagenet/imagenet_mean.binaryproto}"
 LOG_DIR=./output
 VERBOSE_RUN=${VERBOSE_RUN:-0}
 
 SAMPLE_SIZE=1281167
 MAXGPUS="$(nvidia-smi -L | wc -l)"
+
+if [[ ${NGPUS} -gt ${MAXGPUS} ]]; then
+    echo "requested to large number of gpus"
+    exit
+elif [[ ${NGPUS} -lt 1 ]]; then
+    echo "request at least 1 gpu"
+    exit
+fi
 
 case "$NGPUS" in
         1) GPU_OPT=--gpu=0
@@ -25,20 +33,21 @@ case "$NGPUS" in
         ;;
 esac
 
-MAXITER=$((${SAMPLE_SIZE} * ${EPOCHS} / ${BATCHSIZE} / ${NGPUS}))
-
 MODEL_DIR="./models/${MODEL}"
 
 CAFFE_BIN=${CAFFE_BIN:-"/usr/local/bin/caffe"}
 
+FLAG_FP16=
+if [[ ${FP16} -eq 1 ]]; then
+    FLAG_FP16="_fp16"
+fi
+
 SOLVER="iter${MAXITER}_b${BATCHSIZE}_${NGPUS}gpu"
 SOLVER_TMPL_FILE="${MODEL_DIR}/solver_tmpl.prototxt"
 SOLVER_FILE="${MODEL_DIR}/solver_${SOLVER}.prototxt"
-TIMESTAMP=$(date +%m%d%H%M)
-LOG_FILE="${LOG_DIR}/output_${MODEL}_${SOLVER}.${TIMESTAMP}.log"
-
-TRAIN_TMPL_FILE=${MODEL_DIR}/train_val_tmpl_${DATA_KIND}_data.prototxt
-TRAIN_VAL_FILE=${MODEL_DIR}/train_val_${DATA_KIND}_data_b${BATCHSIZE}.prototxt
+LOG_FILE="${LOG_DIR}/output_${MODEL}_${SOLVER}${FLAG_FP16}.log"
+TRAIN_TMPL_FILE=${MODEL_DIR}/train_val${FLAG_FP16}_tmpl.prototxt
+TRAIN_VAL_FILE=${MODEL_DIR}/train_val_b${BATCHSIZE}${FLAG_FP16}.prototxt
 
 # Caffe data layer needs total batch size,
 # whereas dummy data layer needs per gpu batch size
@@ -47,6 +56,7 @@ if [[ $DATA_KIND == fake ]]; then
 else
     TRAIN_BATCHSIZE=$((BATCHSIZE*NGPUS))
 fi
+VAL_BATCHSIZE=${BATCHSIZE}
 
 cleanup() {
     rm -f ${TRAIN_VAL_FILE} ${SOLVER_FILE}
@@ -54,9 +64,12 @@ cleanup() {
 }
 
 # Assuming that @ is not used in train_val files
-cat ${TRAIN_TMPL_FILE} | sed "s/REPLACEBATCHSIZE/${TRAIN_BATCHSIZE}/g" | sed "s@REPLACEIMAGENETMEAN@${IMAGENET_MEAN}@g" | sed "s@REPLACEIMAGENETTRAIN@${IMAGENET_TRAIN}@g" | sed "s@REPLACEIMAGENETVAL@${IMAGENET_VAL}@g" | sed "s@transform_param {@transform_param {${GPU_TRANSFORM}@g" > ${TRAIN_VAL_FILE}
+cat ${TRAIN_TMPL_FILE} | sed "s/REPLACE_TRAIN_BATCH_SIZE/${TRAIN_BATCHSIZE}/g" | sed "s/REPLACE_VAL_BATCH_SIZE/${VAL_BATCHSIZE}/g" | \
+    sed "s@REPLACE_IMAGENET_TRAIN@${IMAGENET_TRAIN}@g" | sed "s@REPLACE_IMAGENET_VAL@${IMAGENET_VAL}@g" | \
+    sed "s@REPLACE_IMAGENET_MEAN@${IMAGENET_MEAN}@g" > ${TRAIN_VAL_FILE}
 
-cat ${SOLVER_TMPL_FILE} | sed "s/REPLACEBATCHSIZE/${BATCHSIZE}/g" | sed "s/REPLACEMAXITER/${MAXITER}/g" | sed "s^REPLACEMODELDIR^${MODEL_DIR}^g" | sed "s^REPLACEDATAKIND^${DATA_KIND}_data^g" > ${SOLVER_FILE}
+cat ${SOLVER_TMPL_FILE} | sed "s/REPLACE_MAX_ITER/${MAXITER}/g" | sed "s^REPLACEMODELDIR^${MODEL_DIR}^g" | \
+    sed "s@REPLACE_TRAIN_VAL_FILE@${TRAIN_VAL_FILE}@g" > ${SOLVER_FILE}
 
 mkdir -p ${LOG_DIR}
 ${CAFFE_BIN}  train --solver=${SOLVER_FILE} ${GPU_OPT} 2>&1 | tee ${LOG_FILE}
